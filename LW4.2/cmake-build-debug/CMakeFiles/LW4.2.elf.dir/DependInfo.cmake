8bd3537f4d3dbc4a97bd8b6f4385f6c2e3f2f918
Maks Rymdenok <maksim.rymdzionak@gmail.com>
2024-03-19 00:27:36 +0300

    Lab 4

---
+++ LW4.2/cmake-build-debug/CMakeFiles/LW4.2.elf.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Startup/startup_stm32g474vetx.s" "D:/University/Computing platforms/workspace/LW4.2/cmake-build-debug/CMakeFiles/LW4.2.elf.dir/Core/Startup/startup_stm32g474vetx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32G474xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Inc"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Inc"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Inc/Legacy"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/include"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/CMSIS/Device/ST/STM32G4xx/Include"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/app_freertos.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/app_freertos.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/app_freertos.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/main.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/main.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/stm32g4xx_hal_msp.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/stm32g4xx_hal_timebase_tim.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/stm32g4xx_hal_timebase_tim.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/stm32g4xx_hal_timebase_tim.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/stm32g4xx_it.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/stm32g4xx_it.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/stm32g4xx_it.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/syscalls.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/sysmem.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Core/Src/system_stm32g4xx.c" "CMakeFiles/LW4.2.elf.dir/Core/Src/system_stm32g4xx.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Core/Src/system_stm32g4xx.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/list.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj.d"
+  "D:/University/Computing platforms/workspace/LW4.2/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj" "gcc" "CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LW4.2/cmake-build-debug/CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj.d
@@ -0,0 +1,33 @@
+CMakeFiles/LW4.2.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj: \
+ D:\University\Computing\ platforms\workspace\LW4.2\Middlewares\Third_Party\FreeRTOS\Source\portable\MemMang\heap_4.c \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/stdlib.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/machine/ieeefp.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/_ansi.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/newlib.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/_newlib_version.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/config.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/features.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/reent.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/_ansi.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/cdefs.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/machine/_default_types.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/_types.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/machine/_types.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/lock.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/machine/stdlib.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/alloca.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\include/FreeRTOS.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/stdint.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/_intsup.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/sys/_stdint.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\Core\Inc/FreeRTOSConfig.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\include/projdefs.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\include/portable.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\include/deprecated_definitions.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\portable\GCC\ARM_CM4F/portmacro.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\include/mpu_wrappers.h \
+ C:/PROGRA~2/ARMGNU~1/13C7F1~1.2RE/arm-none-eabi/include/reent.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\include/task.h \
+ D:\UNIVER~1\COMPUT~1\WORKSP~1\LW4.2\MIDDLE~1\THIRD_~1\FreeRTOS\Source\include/list.h
